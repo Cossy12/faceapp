4872a1acce906aa31b5e76c19450dc18b85c0fbb
MUTUKU <cosmasmutuku001@gmail.com>
2022-07-05 09:40:37 +0300

    fin commit

--- faceapp/settings.py
+++ faceapp/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(96$cm_=!))buw-*)j2dag!vvn0#&2a2w1df4*9_@lsndd+)$y'
+SECRET_KEY =config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
